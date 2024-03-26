01727b5eca017396bf6f1660b316f2eff458b9ed
Jocelyn Remington <jocelynrem@gmail.com>
2024-03-26 21:48:07 +0000

    name function and form

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let counter = 0;
+let heading = document.querySelector('h1');
 
-function count() {
+function plus() {
   counter++;
-  document.querySelector('h3').innerHTML = counter;
+  heading.innerHTML = counter;
+}
+function minus() {
+  counter--;
+  heading.innerHTML = counter;
+}
+
+function updateGreeting(event) {
+  event.preventDefault();
+  var name = document.querySelector('#name').value;
+  document.querySelector('#greeting').innerText = 'Hello, ' + name;
 }
